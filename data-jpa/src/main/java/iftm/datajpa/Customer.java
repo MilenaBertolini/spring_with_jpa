69351227a168400fee8e375ef89d86539362a4ad
MilenaBertolini <milenaarantesb@gmail.com>
2024-03-07 21:24:10 -0300

    first commit

---
+++ data-jpa/src/main/java/iftm/datajpa/Customer.java
@@ -0,0 +1,47 @@
+package iftm.datajpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO) // indica que o id será gerado automaticamente
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {
+
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Customer[id=%d, firstName='%s', lastName='%s']",
+            id, 
+            firstName, 
+            lastName
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
